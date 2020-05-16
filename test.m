70b2ea1bcdf3942acbd3522b85b814d0d0aac235
vuphys <63617302+vuphys@users.noreply.github.com>
2020-05-16 14:35:08 +0100

    Update test.m

--- test.m
+++ test.m
@@ -4,13 +4,13 @@ ffcdata=load('GroundTruth');
 fname=ffcdata.data(:,:,1,1);
 %for i=1:3
 %t=rand(1,1);
-rng(0);
+rng(2);
 %L=fname+randn(size(fname))*0.1;
 K= imnoise(fname,'Gaussian',0,1);
 %LL=psnr(L,fname);
 KK=psnr(K,fname);
-a=vifvec(fname,K);
-sprintf('psnr: %.4f metric: %.8f',KK,a)
+%a=vifvec(fname,K);z
+sprintf('psnr: %.4f',KK)
 imshow([fname,K]);
 %end
 end
