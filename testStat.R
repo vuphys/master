3b982fba1d052792b7cd823e625074283e449f09
vuphys <63617302+vuphys@users.noreply.github.com>
2020-05-18 13:53:32 +0100

    Update testStat.R

--- testStat.R
+++ testStat.R
@@ -6,7 +6,7 @@ library(readr)
 
 # LOAD DATA ################################################
 
-my_data <- read.table("DATA/datUc_10_100.txt", header=T,sep="\t")
+my_data <- read.table("DATA/datFft_50_5000.txt", header=T,sep="\t")
 
 
 my_data
@@ -19,16 +19,16 @@ hist(my_data$img)
 hist(my_data$mean)
 
 # Basic X-Y plot for two quantitative variables
-with(my_data[my_data$img==1,], plot(my_data$lambda, my_data$PSNR))
+with(my_data[my_data$img==1,], plot(my_data$PSNR, my_data$SSIM))
 
 # Add some options
-with(my_data[my_data$img==1,].plot(my_data$alpha_1, my_data$PSNR,
-     pch = 1,         # Solid circle
-     cex = 1,        # Make 150% size
-     col = "#cc0000",  # Red
-     main = "\alpha_0 vs PSNR",
-     xlab = "\alpha_0 value",
-     ylab = "PSNR"))
+with(my_data[my_data$img==1,],plot(my_data$alpha_0, my_data$PSNR,
+     pch = 0.1,         # Solid circle
+     cex = 0.1,        # Make 150% size
+     col = "#cc0000"))  # Red
+     #main = "alpha_1 vs SSIM",
+     #xlab = "alpha_1 value",
+     #ylab = "SSIM"
 
 plot(my_data)
 # CLEAN UP #################################################
