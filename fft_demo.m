cbb5e24be1151416ebef668d47a78a55900ef181
vuphys <63617302+vuphys@users.noreply.github.com>
2020-05-23 22:39:16 +0100

    Update fft_demo.m

--- fft_demo.m
+++ fft_demo.m
@@ -1,12 +1,12 @@
 % This code show a demo run for a set of parameters 
 % based on FFT TGV code
 
-function fft_demo(a,b,c,d,e)
+function fft_demo
 % ADMM parameters
 nite = 100; % number of iterations
 % balancing weights for Total Variation
-alpha = a;  % 1st order
-beta = b; % 2nd order
+alpha = 0.001;  % 1st order
+beta = 0.003; % 2nd order
 
 
 % Loading data   
@@ -19,8 +19,8 @@ GroTru=ffcdata.data(:,:,1,1);
 %I0 = I; % original as the reference
 
 % Adding noise
-rng(e); %reproducibility
-noise_img = imnoise(GroTru,'gaussian',c,d);
+rng(0); %reproducibility
+noise_img = imnoise(GroTru,'gaussian',0,0.01);
 %noise_img = GroTru+randn(size(GroTru))*0.3; % white Gaussian noise added to the image
 	
 
@@ -45,21 +45,16 @@ psnr_tgv = psnr(denoise_img,GroTru);
 % FSIM
 fsim=FeatureSIM(GroTru,denoise_img);
 
-% ESSIM
-essim=ESSIM(GroTru,denoise_img);
-
 %PSNR HVS
 psnr_hvs=psnrhvsm(GroTru,denoise_img);
 
 % PSNR HMA
 psnr_hma=psnrhma(GroTru, denoise_img);
 
-% SR SIM
-srsim=SR_SIM(GroTru, denoise_img);
 
 
 % Displaying results
 
 figure(1), imshow( [GroTru, noise_img, denoise_img] );
-title( sprintf('From the left,  original,  noisy %.6fdB, TGV %.6fdB \n SSIM: %.8f FSIM: %.8f\n ESSIM: %.8f PSNR HVS: %.8f PSNR HMA: %.8f\n MS SSIM: %.8f SR SIM: %.8f', psnr_noisy, psnr_tgv, mssim, fsim,essim,psnr_hvs,psnr_hma, ms_ssim,srsim) );
+title( sprintf('From the left,  original,  noisy %.6fdB, TGV %.6fdB \n SSIM: %.8f FSIM: %.8f\n PSNR HVS: %.8f PSNR HMA: %.8f\n MS SSIM: %.8f', psnr_noisy, psnr_tgv, mssim, fsim,psnr_hvs,psnr_hma, ms_ssim) );
 end
