b016a0b5be401fb6cf703c1758d1eade8a22760e
vuphys <63617302+vuphys@users.noreply.github.com>
2020-05-30 12:51:22 +0100

    Update plotting.m

--- plotting.m
+++ plotting.m
@@ -1,8 +1,16 @@
 function plotting
 
-s=load('DATA/fft_100_2000_par');
-for i=1:100
-    t=s.result.I(i).factor;
-    plot(t(:,1),t(:,9),'.')
+s=load('DATA/he_10_10000_par_2020-05-27T053933');
+%x=zeros(10000,2);
+%for i=1:2
+ %   for j=1:1000
+%x(j,i)=s.result.I(i).factor(j,4)-s.result.I(i).noise_met(2);
+ %   end
+%end
+%x
+for i=8:8
+    t=s.result.I(i);
+    plot(t.factor(:,1),t.factor(:,4),'.')
     hold on
+    plot(t.factor(:,1),t.noise_met(2),'.')
 end
